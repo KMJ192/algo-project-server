f4475bfdddaa9baac460d919c905c7629da67978
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-12-27 00:04:46 +0900

    hot reloading

---
+++ README.md
@@ -0,0 +1,17 @@
+# bot server
+
+### hot reloading
+
+- [document]<https://github.com/watchexec/cargo-watch>
+
+1. install
+
+```
+cargo install cargo-warch
+```
+
+2. run
+
+```
+cargo watch -x 'run algo-project-server'
+```
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,38 @@
+mod random_matching;
+
+// use std::env;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+use random_matching::matching_algorithm::random_matching_build;
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+	HttpResponse::Ok().body("")
 }
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+	HttpResponse::Ok().body(req_body)
 }
 
 async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+	HttpResponse::Ok().body("Hey there!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+	// let mut bind_addr = env::var("BIND_ADDR").expect("Expected a port in the environment");
+	// if bind_addr.len() == 0 {
+	//   bind_addr = String::from("localhost:8080");
+	// }
+
+	HttpServer::new(|| {
+		App::new()
+			// .service(hello)
+			// .service(echo)
+			.service(random_matching_build)
+	})
+	.bind("localhost:8080")?
+	.run()
+	.await
 }
